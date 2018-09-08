e5107643bfbf034f3dfd4ac1da6145bc7dc9549e
manson.zhou <manson.zhou@dzong.com>
2018-09-08 12:21:34 +0800

    d

--- test.sh
+++ test.sh
@@ -4,5 +4,8 @@ kubectl logs -f pod/kube-controller-manager-node -n kube-system
 kubectl logs -f pod/kube-flannel-ds-5whjw -n kube-system
 kubectl logs -f pod/kube-proxy-xdpvt -n kube-system
 kubectl logs -f pod/kube-scheduler-node -n kube-system
+kubectl logs -f pod/kubernetes-dashboard-78dc5f9d6b-vq4dl -n kube-system
 
 
+kubectl get secret -n kube-system | grep dashboard  
+kubectl describe secret kubernetes-dashboard-token-vq4dl -n kube-system 
